dde88970ff9eb9fe1f1302661f9b8600dac1dba9
Leman <LemanE@moodys.com>
2019-06-20 11:22:13 +0200

    el

--- arch_install/chroot_install/chroot.sh
+++ arch_install/chroot_install/chroot.sh
@@ -3,11 +3,15 @@ echo "################################################################"
 echo "###########################  chroot.sh #########################"
 echo "################################################################"
 
+USERNAME=$1
+PASSWORD=$2
+DESKTOP=$3
+HARDWARE=$4
+
 echo "USERNAME is: $USERNAME"
 echo "PASSWORD is: $PASSWORD"
 echo "DESKTOP is: $DESKTOP"
 echo "HARDWARE is: $HARDWARE"
-echo "SWAP_SIZE is: $SWAP_SIZE"
 
 pwd
 
