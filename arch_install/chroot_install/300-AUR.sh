a78a9b91a52382cf1f77ff201ba90d2d4b049ab0
Leman <LemanE@moodys.com>
2019-06-20 15:52:56 +0200

    el

--- arch_install/chroot_install/300-AUR.sh
+++ arch_install/chroot_install/300-AUR.sh
@@ -4,7 +4,10 @@ echo "########################### 300-AUR.sh #########################"
 echo "################################################################"
 
 su eric -c "yay -Syu --noconfirm gnome-shell-extension-dash-to-dock"
+
+pacman -R --noconfirm gnome-terminal
 su eric -c "yay -Syu --noconfirm gnome-terminal-transparency"
+su eric -c "yay -Syu --noconfirm capitaine-cursors"
 
 su eric -c "yay -Syu --noconfirm vscodium-bin"
 /chroot_install/610-vscodium-personal-settings.sh
