53759eea64c0ad6b74db434f223a6bb951fc2825
Leman <LemanE@moodys.com>
2019-06-20 16:45:33 +0200

    el

--- arch_install/chroot_install/500-gnome-settings.sh
+++ arch_install/chroot_install/500-gnome-settings.sh
@@ -3,7 +3,12 @@ echo "################################################################"
 echo "##################### 500-gnome-settings.sh ####################"
 echo "################################################################"
 
+sudo -u eric dbus-launch dconf load / < /chroot_install/personal/gnome-settings.dconf
+
 su eric -c "dconf load / < /chroot_install/personal/gnome-settings.dconf"
+
+su eric -c "mkdir -p /home/eric/.config/gtk-3.0"
 su eric -c "cp /chroot_install/personal/gtk-3.0/settings.ini /home/eric/.config/gtk-3.0/settings.ini"
+
 su eric -c "cp /chroot_install/personal/gtk-2.0/.gtkrc-2.0 /home/eric/.gtkrc-2.0"
 
