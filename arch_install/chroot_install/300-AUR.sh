883278a9cab52eaf13409ae73fe52efb12ca997b
Eric Leman <eric.leman@gmail.com>
2020-04-10 12:13:10 +0200

    virtualdesktopbar

--- arch_install/chroot_install/300-AUR.sh
+++ arch_install/chroot_install/300-AUR.sh
@@ -25,8 +25,9 @@ then
     su eric -c "trizen -Syu --noconfirm plasma5-applets-window-buttons"
     su eric -c "trizen -Syu --noconfirm plasma5-applets-window-title"
     
-    su - eric -c "cd /home/eric && git clone https://github.com/wsdfhjxc/virtual-desktop-bar.git && cd virtual-desktop-bar && mkdir build && cd build && cmake .. && make && sudo make install & cd .. & cd .."
-    rm -rf /home/eric/virtual-desktop-bar
+    su eric -c "trizen -Syu --noconfirm plasma5-applets-virtual-desktop-bar-git"
+    #su - eric -c "cd /home/eric && git clone https://github.com/wsdfhjxc/virtual-desktop-bar.git && cd virtual-desktop-bar && mkdir build && cd build && cmake .. && make && sudo make install & cd .. & cd .."
+    #rm -rf /home/eric/virtual-desktop-bar
 
     
     
