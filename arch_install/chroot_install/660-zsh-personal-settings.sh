ddc84fe34d1d1c84c0c7f444abd0143826989238
ericleman <eric.leman@gmail.com>
2021-04-12 10:58:12 +0200

    Update 660-zsh-personal-settings.sh

--- arch_install/chroot_install/660-zsh-personal-settings.sh
+++ arch_install/chroot_install/660-zsh-personal-settings.sh
@@ -4,7 +4,7 @@ echo "############### 660-zsh-personal-settings.sh #################"
 echo "################################################################"
 
 
-su eric -c "curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh"
+su eric -c "curl -k -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh"
 su eric -c "yes | sh install.sh"
 su eric -c "rm install.sh"
 
